dab7dc28d41e7368606beecdc2c6073b7cc0e979
Kevin Halleran <kevin.halleran@verizon.net>
2024-07-06 17:16:25 -0400

    Made some progress  learning Keras

--- learn.py
+++ learn.py
@@ -232,7 +232,6 @@ class learn:
             dataY.append(dataset[i + look_back, 0])
         return np.array(dataX), np.array(dataY)
     def LSTMRecursion(df, trainId, testId):        
-        file=open('log.txt','w')
         #https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
         tf.random.set_seed(7)
         df=df[['dateRequested','requestedAmount','disasterNumber']]
@@ -248,65 +247,96 @@ class learn:
         df_train=df_train[['dateRequested','requestedAmount']]
         df_test=df_test[['dateRequested','requestedAmount']]
 
-
+        #https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.MinMaxScaler.html
+        #https://stackoverflow.com/questions/62178888/can-someone-explain-to-me-how-minmaxscaler-works
         scaler = MinMaxScaler(feature_range=(0, 1))
         df_train=scaler.fit_transform(df_train)
         df_test=scaler.fit_transform(df_test)
 
         print('reshape into X=t and Y=t+1')
-        file.write('reshape into X=t and Y=t+1')
         look_back = 1
         trainX, trainY = learn.create_dataset(df_train, look_back)
         testX, testY = learn.create_dataset(df_test, look_back)
 
+        #https://www.w3schools.com/python/numpy/numpy_array_reshape.asp
+        #https://numpy.org/doc/stable/reference/generated/numpy.reshape.html
         print('reshape input to be [samples, time steps, features]')
-        file.write('reshape input to be [samples, time steps, features]')
         trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+        print("trainX.shape[0]:" + str(trainX.shape[0]))
+        print("trainX.shape[1]:" + str(trainX.shape[1]))
+
         testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+        print("testX.shape[0]:" + str(testX.shape[0]))
+        print("testX.shape[1]:" + str(testX.shape[1]))
 
         print('create and fit the LSTM network')
-        file.write('create and fit the LSTM network')
+        #https://keras.io/guides/sequential_model/
+        #https://www.tensorflow.org/api_docs/python/tf/keras/Sequential
         model = keras.Sequential()
         model.add(keras.layers.LSTM(4, input_shape=(1, look_back)))
         model.add(keras.layers.Dense(1))
+        model.summary()
+
         model.compile(loss='mean_squared_error', optimizer='adam')
         model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
 
+        #https://keras.io/guides/training_with_built_in_methods/
+        #https://www.tensorflow.org/api_docs/python/tf/keras/Model
+        model.evaluate(testX, testY)
+
+        #This might save me time in the future, really only run it once and save the results since the data is not changing.
+        #https://keras.io/guides/serialization_and_saving/
         print('make predictions')
-        file.write('make predictions')
         trainPredict = model.predict(trainX)
         testPredict = model.predict(testX)
         
         print('invert predictions')
-        file.write('invert predictions')
-        trainPredict = scaler.inverse_transform(trainPredict)
-        trainY = scaler.inverse_transform([trainY])
-        testPredict = scaler.inverse_transform(testPredict)
-        testY = scaler.inverse_transform([testY])
-        
-        print('calculate root mean squared error')
-        file.write('calculate root mean squared error')
-        trainScore = np.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-        print('Train Score: %.2f RMSE' % (trainScore))
-        file.write('Train Score: %.2f RMSE' % (trainScore))
-        testScore = np.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-        print('Test Score: %.2f RMSE' % (testScore))
-        file.write('Test Score: %.2f RMSE' % (testScore))
+        #thank you co-pilot
+        #https://stackoverflow.com/questions/45847006/valueerror-operands-could-not-be-broadcast-together-with-shapes-inverse-trans
+        #https://datascience.stackexchange.com/questions/22488/value-error-operands-could-not-be-broadcast-together-with-shapes-lstm
+        #https://stackoverflow.com/questions/57216718/how-to-inverse-transform-the-predicted-values-in-a-multivariate-time-series-lstm
+        #https://machinelearningmastery.com/multivariate-time-series-forecasting-lstms-keras/
 
-        print('shift train predictions for plotting')
-        file.write('shift train predictions for plotting')
-        trainPredictPlot = np.empty_like(df)
+        testPredict = testPredict.reshape((testX.shape[0], testX.shape[2]))
+        # invert scaling for forecast
+        inv_yhat = np.concatenate((testPredict, testX[:, 1:]), axis=1)
+        inv_yhat = scaler.inverse_transform(inv_yhat)
+        inv_yhat = inv_yhat[:,0]
+        # invert scaling for actual
+        test_y = test_y.reshape((len(test_y), 1))
+        inv_y = np.concatenate((test_y, testX[:, 1:]), axis=1)
+        inv_y = scaler.inverse_transform(inv_y)
+        inv_y = inv_y[:,0]
+        # calculate RMSE
+        rmse = np.sqrt(mean_squared_error(inv_y, inv_yhat))
+        print('Test RMSE: %.3f' % rmse)
+
+        # trainPredict = trainPredict.reshape(testX.shape[0],testX.shape[2])
+
+        # trainPredict=np.concatenate(trainPredict,testX[:,1:],axis=1)
+        # print(trainPredict.shape)
+        # trainPredict = scaler.inverse_transform(trainPredict) #here
+        # trainY = scaler.inverse_transform([trainY])
+        # testPredict = scaler.inverse_transform(testPredict)
+        # testY = scaler.inverse_transform([testY])
+        
+        # print('calculate root mean squared error')
+        # trainScore = np.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+        # print('Train Score: %.2f RMSE' % (trainScore))
+        # testScore = np.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+        # print('Test Score: %.2f RMSE' % (testScore))
+
+        # print('shift train predictions for plotting')
+        trainPredictPlot = np.empty_like(df_train)
         trainPredictPlot[:, :] = np.nan
         trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
         
-        print('shift test predictions for plotting')
-        file.write('shift test predictions for plotting')
-        testPredictPlot = np.empty_like(df)
+        # print('shift test predictions for plotting')
+        testPredictPlot = np.empty_like(df_test)
         testPredictPlot[:, :] = np.nan
         testPredictPlot[len(trainPredict)+(look_back*2)+1:len(df)-1, :] = testPredict
 
         print('plot baseline and predictions')
-        file.write('plot baseline and predictions')
         plt.plot(scaler.inverse_transform(df))
         plt.plot(trainPredictPlot)
         plt.plot(testPredictPlot)
